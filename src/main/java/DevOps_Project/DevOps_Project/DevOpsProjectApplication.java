fdb61ccf97932e77821e93d892ad580697b526c9
dharmateja <dharmateja481@gmail.com>
2025-04-05 10:33:52 -0400

    first commit

---
+++ src/main/java/DevOps_Project/DevOps_Project/DevOpsProjectApplication.java
@@ -0,0 +1,13 @@
+package DevOps_Project.DevOps_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DevOps_Project/DevOps_Project/DevOpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package DevOps_Project.DevOps_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
